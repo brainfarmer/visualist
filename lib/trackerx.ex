9fe90b3271b23d54d4e935e434123da3024d48e0
Dan Nemeth <dnemeth@pivotal.io>
2015-07-16 16:08:17 -0400

    Overrode base::process_url.  Parameterized call_get method.  Updated test for getting epics.  Added test for getting stories.

--- lib/trackerx.ex
+++ lib/trackerx.ex
@@ -4,27 +4,21 @@ defmodule Trackerx do
   """
   use HTTPoison.Base
 
-  @endpoint "https://www.pivotaltracker.com/services/v5"
+  @endpoint "https://www.pivotaltracker.com/services/v5/projects/"
 
   # override base::process_url to create the tracker url
-# def process_url(url) do
-#  @endpoint <> url
-# end
+ def process_url(url) do
+  @endpoint <> url
+ end
 
   # override base::process_request_headers to add tracker API token
   # or override the base::send process and set the token as part
   # of the request
 
   # get the epics for the specified project
-  def get_epics(project_id, api_token) do
+  def call_get(api_token, project_id, url) do
     params = ["X-TrackerToken": api_token]
 
-#   Trackerx.get("/" <> project_id <> "/epics", params)
-    Trackerx.get("https://www.pivotaltracker.com/services/v5/projects/1334474/epics",
-    params)
-
-
-    # make the API call to  Tracker
-    #    {:ok, ~s("fooshizzle")} # return something just to test
+    get(project_id <> url, params)
   end
 end
--- test/integration/tracker_integration_test.exs
+++ test/integration/tracker_integration_test.exs
@@ -4,17 +4,28 @@
 defmodule TrackerIntegrationTest do
   use ExUnit.Case
 
+	
+	@epics_url "/epics"
+	@stories_url "/stories"
+	
   @tracker_mapper_tracker_id "1334474"
-  @ja_api_token 'aa6c95ad3b28fa8520fa75b298a533f4'  # jose's token
+  @ja_api_token 'aa6c95ad3b28fa8520fa75b298a533f4'   # jose's token
 
   @test_project_id  @tracker_mapper_tracker_id
   @test_api_token @ja_api_token
 
   test "GET Epics from tracker" do
-    {status, response} = Trackerx.get_epics(@test_project_id, @test_api_token)
+    {status, response} = Trackerx.call_get(@test_api_token, @test_project_id, @epics_url)
 
     assert status == :ok
     # Raw json should contain epic types
     assert String.match?(response.body, ~r/"kind":"epic"/)
   end
+
+	test "GET Stories from tracker" do
+		{status, response} = Trackerx.call_get(@test_api_token, @test_project_id, @stories_url)
+
+		assert status == :ok
+		assert String.match?(response.body, ~r/"kind":"story"/)
+	end
 end
