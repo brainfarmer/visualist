5b649fda9e85d2008253c5faac0ca9cab14ac0bc
jszod <joe.szod@gmail.com>
2017-02-10 17:58:44 -0500

    Refactoring handle_call :get_story_map with some error handling

--- apps/visualist/lib/mapping/map_server.ex
+++ apps/visualist/lib/mapping/map_server.ex
@@ -35,6 +35,46 @@ defmodule Mapping.MapServer do
   def handle_call({:get_story_map, proj_id, api_token}, _from, state) do
     task_sup_id = :"#{state.name}.MapTaskSupervisor"
 
+    case get_backlog_data(task_sup_id, proj_id, api_token) do
+      {:ok, epics: epics, stories: stories} ->
+	headers = Mapping.StoryMapping.col_headers(epics)
+	columns = Mapping.StoryMapping.stories_by_column_label(headers, stories)
+	story_map = [headers: headers, stories: columns]
+	{:reply, {:ok, story_map}, state}
+
+      {:error, reason} ->
+	{:reply, {:error, reason}, state}
+    end
+    
+    
+    # epics_task = Task.Supervisor.async(task_sup_id,
+    #   fn -> Tracker.Worker.get(:epics, proj_id, api_token) end)
+    # stories_task = Task.Supervisor.async(task_sup_id,
+    #   fn -> Tracker.Worker.get(:stories, proj_id, api_token) end)
+
+    # # Wait for backlog data
+    # {:ok, epic_data} = Task.await(epics_task)
+    # {:ok, story_data} = Task.await(stories_task)
+
+
+    # headers = Mapping.StoryMapping.col_headers(epic_data)
+    # story_map = Mapping.StoryMapping.stories_by_column_label(headers, story_data)
+
+#    the_map = [headers: headers, stories: story_map]
+    
+    # combine headers and stories into a story map
+    #    {:reply, {:error, "not fully implemented"}, state}
+#    {:reply, {:ok, the_map}, state}
+  end
+  
+
+
+  #
+  # Private
+  #
+
+  
+  defp get_backlog_data(task_sup_id, proj_id, api_token) do
     epics_task = Task.Supervisor.async(task_sup_id,
       fn -> Tracker.Worker.get(:epics, proj_id, api_token) end)
     stories_task = Task.Supervisor.async(task_sup_id,
@@ -43,21 +83,39 @@ defmodule Mapping.MapServer do
     {:ok, epic_data} = Task.await(epics_task)
     {:ok, story_data} = Task.await(stories_task)
 
-    headers = Mapping.StoryMapping.col_headers(epic_data)
-    story_map = Mapping.StoryMapping.stories_by_column_label(headers, story_data)
-
-    the_map = [headers: headers, stories: story_map]
-    
-    # combine headers and stories into a story map
-    #    {:reply, {:error, "not fully implemented"}, state}
-    {:reply, {:ok, the_map}, state}
+    {:ok, epics: epic_data, stories: story_data}
   end
+
+
+  # defp generate_headers(:ok, state = %{epic_data: epics}) do
+  #   headers = Mapping.StoryMapping.col_headers(epics)
+  #   %{:ok, state | headers: headers}
+  # end
+
+  
+  # defp generate_map_columns(:ok, state = %{headers: headers, story_data: story_data}) do
+  #   columns = Mapping.StoryMapping.stories_by_column_label(headers, story_data)
+  #   %{:ok, state | columns: columns}
+  # end
+  
+			     
+  # defp combine_into_story_map(:ok, state = %{headers: headers, columns: columns}) do
+  #   [headers: headers, stories: story_map]
+  # end
+
+
+  # defp reply_story_map(:ok, story_map) do
+  #   {:reply, {:ok, story_map}, state}
+  # end
+  
   
 
+  # %{epics: nil, stories: nil, header: nil, column_map}
+  # |> get epics_and_stories(accumulator, proj_id, api_token)
+  # |> gen_column_headers(accoumulator)
+  # |> gen_stories_by_column(accumulator)
 
-  #
-  # Private
-  #
+  # the_map = accumolator.headers, accumulator.columns
   
-
+  
 end
